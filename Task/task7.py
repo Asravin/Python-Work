309f65daa777f92e71e10cdc168a6dfc9747f935
Asravin <maisweedfell@gmail.com>
2023-11-09 16:10:04 +0400

    task7

---
+++ Task/task7.py
@@ -0,0 +1,3 @@
+first_carr = int(input("Введите номер первого вагона: "))
+last_carr = int(input("Введите номер последнего вагона: "))
+print("Количество вагонов в поезде:", first_carr + last_carr - 1)
