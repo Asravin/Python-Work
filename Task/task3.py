61277bb29a88cd8342539b8f212129af69ee2e68
Asravin <maisweedfell@gmail.com>
2023-11-09 12:50:57 +0400

    task3

---
+++ Task/task3.py
@@ -0,0 +1,6 @@
+first_num = int(input("Введите первое целое число: "))
+second_num = int(input("Введите второе целое число: "))
+if first_num > second_num:
+    print("число ", first_num ," больше чем ", second_num)
+else:
+    print("число ", first_num ," меньше чем ", second_num)
