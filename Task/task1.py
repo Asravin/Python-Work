14590864cf0175c93e21dac9b26016fe89bd58f5
Asravin <maisweedfell@gmail.com>
2023-11-09 11:55:40 +0400

    Task1

---
+++ Task/task1.py
@@ -0,0 +1,4 @@
+first_num = int(input("Введите первое число: "))
+second_num = int(input("Введите второе число: "))
+third_num = int(input("Введите третье число: "))
+print("Сумма трех чисел = ", first_num + second_num + third_num) 
