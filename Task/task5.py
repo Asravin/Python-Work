40b114c3e89e320538c4da829eb5721d33ac10e2
Asravin <maisweedfell@gmail.com>
2023-11-09 15:33:54 +0400

    task6

--- Task/task5.py
+++ Task/task5.py
@@ -1,6 +1,3 @@
 dist = int(input("Введите расстояние пути: "))
 speed = int(input("Введите количество км. которое машина проезжает в сутки: "))
-time = dist * 24 / speed
-day = time // 24
-clock = time - day * 24
-print(int(day), "дня", int(clock), "часов")
+print((-dist // speed) * -1, "Дня")
