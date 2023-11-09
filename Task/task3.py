df61f55d7a7f98c8a6e2b2fe4986fc849cc36348
Asravin <maisweedfell@gmail.com>
2023-11-09 13:05:52 +0400

    task3 исправленна

--- Task/task3.py
+++ Task/task3.py
@@ -1,6 +1,6 @@
 first_num = int(input("Введите первое целое число: "))
 second_num = int(input("Введите второе целое число: "))
 if first_num > second_num:
-    print("число ", first_num ," больше чем ", second_num)
+    print("число ",second_num , "меньшее")
 else:
-    print("число ", first_num ," меньше чем ", second_num)
+    print("число ", first_num ," меньшее")
