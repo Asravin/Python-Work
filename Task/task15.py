458100393cdb5a514674d282cfb4e502a4ce5111
Asravin <maisweedfell@gmail.com>
2023-11-14 17:14:18 +0400

    task15

---
+++ Task/task15.py
@@ -0,0 +1,10 @@
+days = int(input("Введиде количество дней: "))
+count = 0
+
+for i in range(days):
+    temp = int(input("Введите показатели температуры: "))
+    if temp > 0:
+        count += 1
+        
+print("Дней с температурой выше 0:", count)
+    
