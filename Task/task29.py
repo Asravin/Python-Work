de30611759bfe9feff5d244deae93434b315e79a
Asravin <maisweedfell@gmail.com>
2023-12-28 17:59:26 +0400

    Задача 29

---
+++ Task/task29.py
@@ -0,0 +1,9 @@
+max = 0
+while True:
+    num = int(input("Введите число: "))
+    if num > max:
+        max = num
+    else:
+        num == 0
+        print("Наибольшее число последовательности:", max)
+        break
